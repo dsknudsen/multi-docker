1ad36640443940b665e2181acb39d6d1e5d37b16
dsknudsen <dsknudsen@gmail.com>
2021-02-04 07:54:47 -0700

    my change

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React (or try)
           </a>
           <Link to="/">Go Home1</Link>
           <Link to="/otherpage">Other Page</Link>
